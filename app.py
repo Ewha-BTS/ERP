74c9e9558ebbeaa319385e2d9e3204da93d27f9a
Yonsoo Kim <48315997+yskim0@users.noreply.github.com>
2021-06-01 17:38:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, request, url_for, jsonify
+from flask_cors import CORS, cross_origin
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from pymongo import ReturnDocument
 import json
 
+
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb+srv://yskim:<password>@cluster0.houen.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+CORS(app)
+app.config["MONGO_URI"] = "mongodb+srv://yskim:11271127@cluster0.houen.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 mongo = PyMongo(app)
 
 ALLOWED_EXTENSIONS = set(['json', 'tsv', 'csv'])
@@ -24,15 +27,37 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
+    print(11111)
     file = request.files['file'] # file 타입의 name
+    # file = request.form['file']
+    
+    # print(2222)
+    print(file) # OK 
     # filename = secure_filename(file.filename)
     if file and allowed_file(file.filename):
+        print('---1')
         vizspec = [] # 빈 Array // inference 과정에서 생성할 예정이므로.
+        print('---2')
         mongo.save_file(file.filename, file)
+        print('---3')
+
         # mongo.db.users.insert({'filename' : file.filename, 'vizspec' : vizspec})
-        _id = mongo.db.users.insert({'filename' : file.filename, 'vizspec' : vizspec})
+        # _id = mongo.db.users.insert({'filename' : file.filename, 'vizspec' : vizspec})
+        _id = mongo.db.users.insert_one({'filename' : file.filename, 'vizspec' : vizspec})
+
+        print(_id)
+        print('---4')
+
+        print('0----')
+        print(_id)
     # return 'Done!'
-    return jsonify(str(ObjectId(_id)))
+        print('---5')
+        
+        # res = jsonify(str((_id)))
+        # res = jsonify(_id.inserted_id)
+
+    # return res
+    return str(_id.inserted_id)
 
 @app.route('/file/<filename>')
 def file(filename):
@@ -51,7 +76,7 @@ def inference(user_id):
     
     # data2vis가 barley.json 가지고 만든 Vega-lite 코드
     for i in range(10):
-        viz_path = 'erp-server/barley/barley'+str(i+1)+'.json'
+        viz_path = 'barley/barley'+str(i+1)+'.json'
         with open(viz_path) as f:
             tmp_data = json.load(f)
             vizspec.append(tmp_data)
@@ -61,9 +86,13 @@ def inference(user_id):
     # user_id에 맞는 collection 가져와서 vizspec 부분 업데이트
     mongo.db.users.update({'_id' : ObjectId(user_id)}, {'$set' : {'vizspec' : vizspec}})
 
+    res = jsonify({'vizspec' : vizspec})
+    # res.headers.add('Access-Control-Allow-Origin', '*')
+    # res.headers.add('Access-Control-Allow-Headers', "*")
+    # res.headers.add('Access-Control-Allow-Methods', "*")
     # return 'Generated Plots.'
-    return jsonify({'message' : 'vizspec Updated'})
+    return res
 
 
 if __name__ == '__main__':
-    app.run(debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=5018, debug=True, threaded=True)

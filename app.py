2c9f1c585331033bb690de929ba4dae0f97b91a5
JungHyun <leahincom@gmail.com>
2021-06-01 17:40:48 +0900

    update app.py for heroku

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, url_for, jsonify
-from flask_cors import CORS, cross_origin
+# from flask_cors import CORS, cross_origin
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from pymongo import ReturnDocument
@@ -7,7 +7,7 @@ import json
 
 
 app = Flask(__name__)
-CORS(app)
+# CORS(app)
 app.config["MONGO_URI"] = "mongodb+srv://yskim:11271127@cluster0.houen.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 mongo = PyMongo(app)
 
